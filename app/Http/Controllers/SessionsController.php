0f9196fc61235ee23d29ddf001f89d88fb8ccd96
Highness Prince <13616520998@163.com>
2018-06-02 12:00:18 +0800

    添加用户删除功能。

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Http\Requests;
+use Auth;
+use Illuminate\Http\Request;
+
+class SessionsController extends Controller {
+    
+    public function __construct() {
+        
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
+    
+    //
+    public function create() {
+        
+        return view('sessions.create');
+    }
+    
+    public function store(Request $request) {
+        
+        $credentials = $this->validate($request, [
+            'email'    => 'required|email|max:255',
+            'password' => 'required'
+        ]);
+        
+        $accredited = Auth::attempt($credentials, $request->has('remember'));
+        if ( ! $accredited) {
+            
+            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+            
+            return redirect()->back();
+        }
+        
+        session()->flash('success', '欢迎回来！');
+        
+        return redirect()->intended(route('users.show', [Auth::user()]));
+    }
+    
+    public function destroy() {
+        
+        Auth::logout();
+        session()->flash('success', '您已成功退出！');
+        
+        return redirect()->route('login');
+    }
+}
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -11,5 +11,8 @@
     @yield('content')
     @include('layouts._footer')
 </div>
+
+<script src="/js/app.js"></script>
+
 </body>
 </html>
