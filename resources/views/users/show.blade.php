c07a7abfbf9a1457f1e82a83a69ae81b2d0a3f27
Highness Prince <13616520998@163.com>
2018-06-04 17:08:30 +0800

    Add user following

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,8 +1,6 @@
 @extends('layouts.default')
-
 @section('title', $user->name)
 @section('content')
-
     <div class="row">
         <div class="col-md-offset-2 col-md-8">
             <div class="col-md-12">
@@ -10,9 +8,16 @@
                     <section class="user_info">
                         @include('shared._user_info', ['user' => $user])
                     </section>
+                    <section class="stats">
+                        @include('shared._stats', ['user' => $user])
+                    </section>
                 </div>
             </div>
             <div class="col-md-12">
+                @if (Auth::check())
+                    @include('users._follow_form')
+                @endif
+
                 @if (count($statuses) > 0)
                     <ol class="statuses">
                         @foreach ($statuses as $status)
@@ -24,5 +29,4 @@
             </div>
         </div>
     </div>
-
-@endsection
+@stop
